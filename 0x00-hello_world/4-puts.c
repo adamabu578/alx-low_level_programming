4a8feb60c5ef7a348176282bb37ecef50b2a6c89
adamuabu <abukamsulum01@gmail.com>
2023-06-16 14:03:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Description: 'To be able to run my program'
- * Return: Always 0 (Success) 
- */  
-int main(void) 
-{  
-puts("\"Programming is like building a multilingual puzzle");
-return (0); 
+ * Description: 'To be able to run any program'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
