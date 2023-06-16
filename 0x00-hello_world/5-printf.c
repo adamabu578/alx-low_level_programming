586e1c9a781bd8016a07987a9d0ec431492a70c7
adamuabu <abukamsulum01@gmail.com>
2023-06-16 15:39:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry poit 
+ * main - Entry point
  * Description: 'To be able to run any program'
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
