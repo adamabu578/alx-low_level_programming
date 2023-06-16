78ee5ec627282befd14cf3b7faeef327e45ad7a1
adamuabu <abukamsulum01@gmail.com>
2023-06-16 03:39:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
  * main - entry point
  *
  * Return: Always 0 (Success) 
- */ 
+ */  
 int main(void) 
 {
-	puts("\"Programming is like building a multilingual puzzle");
-return 0;
+	puts("Programming is like building a multilingual puzzle");
+return 0; 
 }
 
 
