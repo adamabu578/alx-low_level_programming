2e8559698a6a505ea9ceecaa768d48725466a158
adamuabu <abukamsulum01@gmail.com>
2023-06-23 20:47:46 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - straight-line
+ * @n: No of times 
+ * Return:Always 0
+ */
+
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
