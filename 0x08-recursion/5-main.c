8c0c6f8d2c2bc24c362298fec1f56fe5aef04c26
adamuabu <abukamsulum01@gmail.com>
2023-07-05 16:42:01 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-main.c
+++ 0x08-recursion/5-main.c
@@ -1,37 +1,26 @@
+#include "main.h"
+#include <stdio.h>
 
-You just released the advanced tasks of this project. Have fun!
-0x08. C - Recursion
-C
-Recursion
- By: Julien Barbier
-  Weight: 1
-   Project will start Jul 5, 2023 6:00 AM, must end by Jul 6, 2023 6:00 AM
-    Checker was released at Jul 5, 2023 6:00 AM
-     An auto review will be launched at the deadline
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
 
-
-     Resources
-     Read or watch:
-
-     0x08. Recursion, introduction
-     What on Earth is Recursion?
-     C - Recursion
-     C Programming Tutorial 85, Recursion pt.1
-     C Programming Tutorial 86, Recursion pt.2
-     Learning Objectives
-     At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
-
-     General
-     What is recursion
-     How to implement recursion
-     In what situations you should implement recursion
-     In what situations you shouldn’t implement recursion
-     Copyright - Plagiarism
-     You are tasked to come up with solutions for the tasks below yourself to meet with the above learning objectives.
-     You will not be able to meet the objectives of this or any following project by copying and pasting someone else’s work.
-     You are not allowed to publish any content of this project.
-     Any form of plagiarism is strictly forbidden and will result in removal from the program.
-     Requirements
-     General
-     Allowed editors: vi, vim, emacs
-     All your files will be compiled on Ubuntu 20
+    r = _sqrt_recursion(1);
+    printf("%d\n", r);
+    r = _sqrt_recursion(1024);
+    printf("%d\n", r);
+    r = _sqrt_recursion(16);
+    printf("%d\n", r);
+    r = _sqrt_recursion(17);
+    printf("%d\n", r);
+    r = _sqrt_recursion(25);
+    printf("%d\n", r);
+    r = _sqrt_recursion(-1);
+    printf("%d\n", r);
+    return (0);
+}
